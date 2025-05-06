8bab633d23fdf420d8d4128cd5180ef8cba4ac08
Mahalakshmikudipudi <mahalakshmiguthula125@gmail.com>
2025-05-06 12:26:15 +0530

    Create user table using mongoose

--- app.js
+++ app.js
@@ -19,14 +19,14 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('681865cd3a8a678a773e47dd')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('6819abbc44fdecff7e192f0c')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -35,8 +35,21 @@ app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://mahalakshmiguthula125:41FkP94zrgTlrbgC@cluster0.5z4dqka.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
   .then(result => {
-    app.listen(3000);
-    console.log('Connected to MongoDB');
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Mahalakshmi',
+          email: 'maha@gmail.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+
+      }
+      app.listen(3000);
+      console.log('Connected to MongoDB');
+    })
   })
   .catch(err => {
     console.log(err);
